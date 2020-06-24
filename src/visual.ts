10e01ec6ec16d7f5e6710d81a9fbabf613bfb8d3
Your Name <you@example.com>
2020-06-23 18:02:51 -0600

    bar visualization with categorical data and measure data

--- src/visual.ts
+++ src/visual.ts
@@ -38,8 +38,14 @@ import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 
 import { VisualSettings } from "./settings";
+import DataViewTableRow = powerbi.DataViewTableRow;
 type Selection<T extends d3.BaseType> = d3.Selection<T, any, any, any>;
 import * as d3 from "d3";
+
+export interface TestItem {
+  Country: string;
+  Gross_Sales: number;
+}
 
 /**
  * Interface for BarCharts viewmodel.
@@ -68,23 +74,89 @@ interface BarChartDataPoint {
 export class Visual implements IVisual {
   private target: HTMLElement;
   private myVisualProp: boolean;
+  private svg: Selection<SVGElement>;
+  private g: Selection<SVGElement>;
+  private margin = { top: 20, right: 20, bottom: 200, left: 70 };
 
   constructor(options: VisualConstructorOptions) {
-    var captionArea = document.createElement("div");
-    captionArea.innerHTML = "This is test chart";
-    options.element.appendChild(captionArea);
-    this.target = document.createElement("div");
-    options.element.appendChild(this.target);
-
-    this.myVisualProp = false;
+    this.svg = d3.select(options.element).append("svg");
+    this.g = this.svg.append("g");
   }
 
   public update(options: VisualUpdateOptions) {
-    // This example just shows the selected property in visual.
-    this.myVisualProp = options.dataViews[0].metadata.objects["myCustomObj"][
-      "myprop"
-    ] as boolean;
-    this.target.innerHTML = "Custom Prop is " + this.myVisualProp;
+    var _this = this;
+
+    _this.svg.attr("width", options.viewport.width);
+    _this.svg.attr("height", options.viewport.width);
+
+    var gHeight =
+      options.viewport.height - _this.margin.top - _this.margin.bottom;
+
+    var gWidth =
+      options.viewport.width - _this.margin.right - _this.margin.left;
+
+    _this.g.attr("width", gWidth);
+    _this.g.attr("height", gHeight);
+    _this.g.attr(
+      "transform",
+      "translate(" + _this.margin.left + "," + _this.margin.top + ")"
+    );
+
+    var dat = Visual.converter(options.dataViews[0].table.rows);
+
+    var xScale = d3
+      .scaleBand()
+      .domain(dat.map(d => d.Country))
+      .rangeRound([0, gWidth])
+      .padding(0.1);
+
+    var yMax = d3.max(dat, d => d.Gross_Sales + 10);
+
+    var yScale = d3
+      .scaleLinear()
+      .domain([0, yMax])
+      .range([gHeight, 0]);
+
+    _this.svg.selectAll(".axis").remove();
+    _this.svg.selectAll(".bar").remove();
+
+    var xAxis = d3.axisBottom(xScale);
+    _this.g
+      .append("g")
+      .attr("class", "x axis")
+      .style("fill", "black")
+      .attr("transform", "translate(0," + (gHeight - 1) + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.6em")
+      .attr("transform", "rotate(-90)");
+
+    var yAxis = d3.axisLeft(yScale);
+    _this.g
+      .append("g")
+      .attr("class", "y axis")
+      .style("fill", "black")
+      .call(yAxis);
+
+    var shapes = _this.g
+      .append("g")
+      .selectAll(".bar")
+      .data(dat);
+
+    shapes
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("fill", "green")
+      .attr("stroke", "black")
+      .attr("x", d => xScale(d.Country))
+      .attr("width", xScale.bandwidth())
+      .attr("y", d => yScale(d.Gross_Sales))
+      .attr("height", d => gHeight - yScale(d.Gross_Sales));
+
+    shapes.exit().remove();
   }
 
   public enumerateObjectInstances(
@@ -110,5 +182,19 @@ export class Visual implements IVisual {
 
   public destroy(): void {
     //TODO: Perform any cleanup tasks here
+  }
+
+  public static converter(rows: DataViewTableRow[]): TestItem[] {
+    var resultData: TestItem[] = [];
+
+    for (var i = 0; i < rows.length; i++) {
+      var row = rows[i];
+      resultData.push({
+        Country: row[0] as string,
+        Gross_Sales: row[1] as number
+      });
+    }
+
+    return resultData;
   }
 }
